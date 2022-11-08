93a7be34a7276d8cc21569f006b75396825a46e7
DiZu7 <dianadovhanyuk@gmail.com>
2022-11-08 16:00:14 +0200

    test

---
+++ lesson1-react-intro/task3/webpack.common.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'review_build'),
+    filename: '[name].js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
