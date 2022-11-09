9c8458c4f7ad39e78ee0e279fc92e36eebde0187
DiZu7 <dianadovhanyuk@gmail.com>
2022-11-09 23:10:08 +0200

    added init files

---
+++ lesson3-components-props/task3/src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+import Comment from './Comment';
+
+const rootElem = document.querySelector('#root');
+
+const userInfo = {
+  name: 'Tom',
+  avatarUrl: 'https://avatars.dicebear.com/api/adventurer-neutral/mail%40ashallendesign.co.uk.svg',
+};
+
+ReactDOM.render(
+  <Comment user={userInfo} text="Good job!" date={new Date('2022-11-09T11:30:19.566Z')} />,
+  rootElem,
+);
