4196ce2b10d39b93ce57989c6fdb8681ca29a71e
DiZu7 <dianadovhanyuk@gmail.com>
2022-11-11 23:29:25 +0200

    added init files

---
+++ lesson5-events/task2/src/GoodButton.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './index.scss';
+
+class GoodButton extends Component {
+  constructor() {
+    super();
+  }
+  handleClick(e) {
+    alert(e.target.textContent);
+  }
+
+  render() {
+    return (
+      <button className="fancy-button" onClick={this.handleClick}>
+        Click me!
+      </button>
+    );
+  }
+}
+
+export default GoodButton;
