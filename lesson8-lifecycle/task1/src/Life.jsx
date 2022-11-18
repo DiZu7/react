2c4edbdf9e191abb9a67dbc36bf9fcd14dd9584e
DiZu7 <dianadovhanyuk@gmail.com>
2022-11-18 12:25:53 +0200

    test

--- lesson8-lifecycle/task1/src/Life.jsx
+++ lesson8-lifecycle/task1/src/Life.jsx
@@ -4,27 +4,40 @@ class Life extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      number: Math.round(Math.random() * 100),
+      visible: false,
+    };
     console.log('constructor: good place to create state');
   }
 
   componentDidMount() {
+    this.setState({
+      visible: true,
+      number: Math.round(Math.random() * 100),
+    });
+
     console.log('componentDidMount: API calls, subscriptions');
   }
+
   shouldComponentUpdate(nextProps, nextState) {
     console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
-    return nextProps.number;
+    return nextProps.number !== this.state.number;
   }
   componentDidUpdate(prevProps, prevState) {
     console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
   }
 
   componentWillUnmount() {
+    this.setState({
+      visible: false,
+    });
     console.log('componentWillUnmount: cleanup before DOM related to component will be removed');
   }
 
   render() {
     console.log('return React element to build DOM');
-    return <div>{this.props.number}</div>;
+    return <div>{this.state.visible && this.state.number}</div>;
   }
 }
 export default Life;
