c1eedfa07a0c854e67384c8bef0d30d56cf97edc
DiZu7 <dianadovhanyuk@gmail.com>
2022-11-18 14:16:20 +0200

    test

--- lesson8-lifecycle/task1/src/Life.jsx
+++ lesson8-lifecycle/task1/src/Life.jsx
@@ -3,39 +3,28 @@ import React from 'react';
 class Life extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      number: Math.round(Math.random() * 100),
-    };
     console.log('constructor: good place to create state');
   }
 
   componentDidMount() {
-    this.interval = setInterval(() => {
-      this.setState({
-        number: Math.round(Math.random() * 100),
-      });
-    }, 1000);
-
     console.log('componentDidMount: API calls, subscriptions');
   }
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
-    return nextProps.number !== this.state.number;
+    return nextProps.number !== this.props.number;
   }
   componentDidUpdate(prevProps, prevState) {
     console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
   }
 
   componentWillUnmount() {
-    clearInterval(this.interval);
     console.log('componentWillUnmount: cleanup before DOM related to component will be removed');
   }
 
   render() {
     console.log('return React element to build DOM');
-    return <div>{this.state.number}</div>;
+    return <div>{this.props.number}</div>;
   }
 }
 export default Life;
