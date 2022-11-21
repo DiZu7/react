10f488bce11af44873ac6c4cd2c257e38e3e8dd2
DiZu7 <dianadovhanyuk@gmail.com>
2022-11-21 23:05:58 +0200

    test

--- lesson9-forms/task1-hw/src/UsersList.jsx
+++ lesson9-forms/task1-hw/src/UsersList.jsx
@@ -5,23 +5,23 @@ import Filter from './Filter';
 
 class UsersList extends React.Component {
   state = {
-    value: '',
-    // count: this.props.users.length,
+    filterText: '',
+    count: this.props.users.length,
   };
 
   handleChange = e => {
     this.setState({
-      value: e.target.value,
+      filterText: e.target.value,
     });
   };
 
-  filterUsers = (users, filterText) => {
-    if (filterText === '') {
-      return users;
-    }
+  // filterUsers = (users, filterText) => {
+  //   if (filterText === '') {
+  //     return users;
+  //   }
 
-    return users.filter(({ name }) => name.includes(filterText));
-  };
+  //   return users.filter(({ name }) => name.includes(filterText));
+  // };
 
   // handleChangeCount = (users, filterText) => {
   //   this.setState({
@@ -30,22 +30,30 @@ class UsersList extends React.Component {
   // };
 
   render() {
-    const renderedList = this.filterUsers(this.props.users, this.state.value).map(user => (
-      <User key={user.id} {...user} />
-    ));
+    // const renderedList = this.filterUsers(this.props.users, this.state.value).map(user => (
+    //   <User key={user.id} {...user} />
+    // ));
 
     // if (this.state.value === '') {
     //   return users;
     // }
 
+    const newArr = this.props.users.filter(({ name }) =>
+      name.toUpperCase().includes(this.state.filterText.toUpperCase()),
+    );
+
     return (
       <div>
         <Filter
-          filterText={this.state.value}
-          count={renderedList.length}
+          filterText={this.state.filterText}
+          count={newArr.length}
           onChange={this.handleChange}
         />
-        <ul className="users">{renderedList}</ul>
+        <ul className="users">
+          {this.state.filterText === ''
+            ? this.props.users.map(user => <User key={user.id} {...user} />)
+            : newArr.map(user => <User key={user.id} {...user} />)}
+        </ul>
       </div>
     );
   }
