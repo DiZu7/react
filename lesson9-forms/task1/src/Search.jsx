28a0d7abfd8b3fc84b300ff769cf4ec774ba0722
DiZu7 <dianadovhanyuk@gmail.com>
2022-11-21 15:51:29 +0200

    test

---
+++ lesson9-forms/task1/src/Search.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './index.scss';
+
+class Search extends React.Component {
+  state = {
+    value: '',
+  };
+
+  handleChange = e => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  search = e => {
+    e.preventDefault;
+    alert(`Search text: ${this.state.value}`);
+  };
+
+  render() {
+    return (
+      <form className="search" onSubmit={this.search}>
+        <input
+          type="text"
+          className="search__input"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <button className="search__button" type="submit">
+          Search
+        </button>
+      </form>
+    );
+  }
+}
+export default Search;
