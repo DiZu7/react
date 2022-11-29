a802aaf6e5c0c3f61f5bbece44109513121a1e2c
DiZu7 <dianadovhanyuk@gmail.com>
2022-11-29 21:27:34 +0200

    test

--- lesson14-react-hooks/task1/src/App.jsx
+++ lesson14-react-hooks/task1/src/App.jsx
@@ -1,36 +1,9 @@
 import React from 'react';
-import EvenNumbers from './EvenNumbers';
+import Counter from './Counter';
 import './index.scss';
-import Numbers from './Numbers';
-import OddNumbers from './OddNumbers';
 
-class App extends React.Component {
-  state = {
-    number: 0,
-  };
-
-  componentDidMount() {
-    this.intervalId = setInterval(() => {
-      this.setState({
-        number: this.state.number + 1,
-      });
-    }, 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.intervalId);
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <EvenNumbers number={this.state.number} />
-        <Numbers title="All numbers" number={this.state.number} />
-        <OddNumbers number={this.state.number} />
-        <Numbers title="Just 17" number={17} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <Counter />;
+};
 
 export default App;
