c403c82a661a2039eec5cca3d61d0bd024d21ddc
DiZu7 <dianadovhanyuk@gmail.com>
2023-01-14 13:43:52 +0200

    tested other student's answer

--- lesson5-events/task1-hw/src/Toggler.jsx
+++ lesson5-events/task1-hw/src/Toggler.jsx
@@ -7,26 +7,51 @@
 // 3. add handler to button
 // 4. Add condition if status true - return off, else on
 
+// import React from 'react';
+// import './index.scss';
+
+// class Toggler extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isToggleOff: true,
+//     };
+//   }
+
+//   toggleStatus = () => {
+//     this.setState(state => ({
+//       isToggleOff: !state.isToggleOff,
+//     }));
+//   };
+//   render() {
+//     return (
+//       <div className="toggler" onClick={this.toggleStatus}>
+//         {this.state.isToggleOff ? 'Off' : 'On'}
+//       </div>
+//     );
+//   }
+// }
+
+// export default Toggler;
+
 import React from 'react';
-import './index.scss';
 
 class Toggler extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isToggleOff: true,
+      status: 'Off',
     };
   }
-
-  toggleStatus = () => {
-    this.setState(state => ({
-      isToggleOff: !state.isToggleOff,
-    }));
+  toggler = () => {
+    this.setState({
+      status: this.state.status === 'Off' ? 'On' : 'Off',
+    });
   };
   render() {
     return (
-      <div className="toggler" onClick={this.toggleStatus}>
-        {this.state.isToggleOff ? 'Off' : 'On'}
+      <div className="toggler" onClick={this.toggler}>
+        {this.state.status}
       </div>
     );
   }
