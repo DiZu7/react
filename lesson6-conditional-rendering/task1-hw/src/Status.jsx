720b9c049131339026c6600c783d6986aee86f12
DiZu7 <dianadovhanyuk@gmail.com>
2023-02-05 22:51:57 +0200

    test

--- lesson6-conditional-rendering/task1-hw/src/Status.jsx
+++ lesson6-conditional-rendering/task1-hw/src/Status.jsx
@@ -3,8 +3,24 @@ import './index.scss';
 import Online from './Online';
 import Offline from './Offline';
 
-const Status = ({ isOnline }) => {
-  return <div className="status">{isOnline ? <Online /> : <Offline />}</div>;
-};
+class Status extends React.Component {
+  state = {
+    isOnline: false,
+  };
+
+  goOnline = () => {
+    this.setState({
+      isOnline: true,
+    });
+  };
+
+  render() {
+    return (
+      <div className="status">
+        {this.state.isOnline ? <Online /> : <Offline goOnline={this.goOnline} />}
+      </div>
+    );
+  }
+}
 
 export default Status;
